ff67acf5a09273d86ba7d1c25ee68224c2a62635
dineshvadalacompose <dinesh@compose.co.in>
2026-03-17 09:52:37 +0530

    style: format FOLDERS_TO_GENERATE for better readability

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -48,7 +48,14 @@ const COLLECTION_LOCAL_PATH = path.resolve(__dirname, '../collection.json');
 
 const OUTPUT_DIR = path.resolve(__dirname, '../nodes/Commercetools/generated');
 
-const FOLDERS_TO_GENERATE = ['Products', 'Customers', 'Carts', 'Orders', 'Business-units', 'Categories']; // For Actions;
+const FOLDERS_TO_GENERATE = [
+	'Products',
+	'Customers',
+	'Carts',
+	'Orders',
+	'Business-units',
+	'Categories',
+]; // For Actions;
 
 const RESOURCES_TO_GENERATE = ['product', 'customer', 'cart', 'order', 'business-unit', 'category']; // For Triggers;
 
