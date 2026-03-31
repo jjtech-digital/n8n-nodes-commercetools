df73149556ae849b5f090b4f50aaceff4fbee4e4
Koteswara Rao Mudila <koteswara@compose.co.in>
2026-03-31 12:11:47 +0530

    changes

--- nodes/Commercetools/generated/subscription.properties.ts
+++ nodes/Commercetools/generated/subscription.properties.ts
@@ -2,7 +2,7 @@
  * subscription.properties.ts
  *
  * AUTO-GENERATED — DO NOT EDIT MANUALLY
- * Generated at: 2026-03-31T06:15:04.376Z
+ * Generated at: 2026-03-31T06:31:21.948Z
  */
 
 import { INodeProperties } from 'n8n-workflow';
